d8fec101859c4dce6dccbce5c589661b941bd944
Benjamin Hahn <hahnb@student.ethz.ch>
2018-12-06 15:06:01 +0100

    now using DSO tf (and not ground truth tf from rosbag)

--- include/ROSOutputWrapper.h
+++ include/ROSOutputWrapper.h
@@ -4,7 +4,11 @@
 #include <pcl_ros/point_cloud.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <pointmatcher_ros/transform.h>
+
 
 #include "util/NumType.h"
 #include "IOWrapper/OutputWrapper/SampleOutputWrapper.h"
@@ -21,6 +25,7 @@ public:
     printf("OUT: Created ROSOutputWrapper\n");
     pointcloud_info_pub = nh.advertise<std_msgs::String>("pointcloud_info", 1000);
     pointcloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB>>("pointcloud", 1000);
+    cam_pose_pub = nh.advertise<tf::tfMessage>("tf", 1000);
     printf("OUT: generated publisher!\n");
   }
 
@@ -39,22 +44,21 @@ public:
     pcl::PointCloud<pcl::PointXYZRGB> cloud;
     int num_pt_marg = 0;
     Mat44 const T_newestF_world =  frames.back()->shell->camToWorld.matrix().inverse();
-    // Mat44 T_world_newestF = frames.back()->shell->camToWorld.matrix();
-    // std::cout << "T_world_newestF * T_newestF_world:\n" << T_world_newestF * T_newestF_world << std::endl;
 
     for (FrameHessian* f : frames) {
+      // if (f != frames.begin()[1] && f != frames.begin()[2]) {
+      //   continue;
+      // }
       Mat44 T_newestF_currF = T_newestF_world * f->shell->camToWorld.matrix();
-      // std::cout << "T_newestF_world:\n" << T_newestF_world << std::endl;
-      // std::cout << "f->shell->camToWorld.matrix():\n" << f->shell->camToWorld.matrix() << std::endl;
-      // std::cout << "T_newestF_currF:\n" << T_newestF_currF << std::endl;
-        // printf("OUT: KF %d (%s) (id %d, tme %f): %d active, %d marginalized, %d immature points. CameraToWorld:\n",
-        //        f->frameID,
-        //        final ? "final" : "non-final",
-        //        f->shell->incoming_id,
-        //        f->shell->timestamp,
-        //        (int)f->pointHessians.size(),
-        //        (int)f->pointHessiansMarginalized.size(),
-        //        (int)f->immaturePoints.size());
+      ROS_WARN("distance from frame %d to current frame %d: (%f, %f, %f) --> norm: %f and %f and %f\n",
+               f->frameID,
+               frames.back()->frameID,
+               T_newestF_currF(0, 3),
+               T_newestF_currF(1, 3),
+               T_newestF_currF(2, 3),
+               T_newestF_currF.block<3, 1>(0, 3).norm(),
+               T_newestF_world.block<3, 1>(0, 3).norm(),
+               f->shell->camToWorld.matrix().block<3, 1>(0, 3).norm());
 
       float cx = HCalib->cxl() /* * SCALE_C */;
       float cy = HCalib->cyl() /* * SCALE_C */;
@@ -65,8 +69,15 @@ public:
       float cxi = -cx / fx;
       float cyi = -cy / fy;
 
-      // std::cout << "cam calib: " << fx << " " << fy << " " << cx << " " << cy << std::endl;
-      // std::cout << "cam calib inv: " << fxi << " " << fyi << " " << cxi << " " << cyi << std::endl;
+      // // draw all cam poses
+      // pcl::PointXYZRGB pt_w(255, 255, 255);
+      // Vec4 cam_pt = {0, 0, 0, 1};
+      // cam_pt = T_newestF_currF * cam_pt * rescale_factor;
+      // pt_w.x = cam_pt[0];
+      // pt_w.y = cam_pt[1];
+      // pt_w.z = cam_pt[2];
+      // cloud.push_back(pt_w);
+      // std::cout << "pt_w: " << pt_w.x << " " << pt_w.y << " " << pt_w.z << std::endl;
 
       for (PointHessian* p : f->pointHessians) {
         Vec4 pt_cam;
@@ -80,18 +91,7 @@ public:
         pt_cam[1] = y;
         pt_cam[2] = z;
         pt_cam[3] = 1.f;
-        // std::cout << "pt_cam: " << pt_cam[0] << " " << pt_cam[1] << " " << pt_cam[2]
-        // << " " << pt_cam[3] << " coming from " << p->u << " " << p->v << " and p->idepth = " << p->idepth <<
-        // std::endl;
-        // pt_cam[0] = (p->u - cx) / fx / p->idepth;
-        // pt_cam[1] = (p->v - cy) / fy / p->idepth;
-        // pt_cam[2] = 1 / p->idepth;
-        // pt_cam[3] = 1;
-        // pt_cam = f->shell->camToWorld.matrix() * pt_cam;
-        // if (num_pt_marg % 100 == 0) {
-        //   std::cout << "f->shell->camToWorld.matrix3x4(): " << f->shell->camToWorld.matrix3x4() << std::endl;
-        // }
-        Vec4 pt_world = T_newestF_currF * pt_cam * 20.6128;
+        Vec4 pt_world = T_newestF_currF * pt_cam * rescale_factor;
 
         pcl::PointXYZRGB pt_w(255, 255, 255);
         pt_w.x = pt_world[0];
@@ -114,17 +114,7 @@ public:
         pt_cam[1] = y;
         pt_cam[2] = z;
         pt_cam[3] = 1.f;
-        // std::cout << "pt_cam: " << pt_cam[0] << " " << pt_cam[1] << " " << pt_cam[2]
-                  // << " " << pt_cam[3] << " coming from " << p->u << " " << p->v << " and p->idepth = " << p->idepth << std::endl;
-        // pt_cam[0] = (p->u - cx) / fx / p->idepth;
-        // pt_cam[1] = (p->v - cy) / fy / p->idepth;
-        // pt_cam[2] = 1 / p->idepth;
-        // pt_cam[3] = 1;
-        // pt_cam = f->shell->camToWorld.matrix() * pt_cam;
-        // if (num_pt_marg % 100 == 0) {
-        //   std::cout << "f->shell->camToWorld.matrix3x4(): " << f->shell->camToWorld.matrix3x4() << std::endl;
-        // }
-        Vec4 pt_world = T_newestF_currF * pt_cam * 20.6128;
+        Vec4 pt_world = T_newestF_currF * pt_cam * rescale_factor;
 
         pcl::PointXYZRGB pt_w(255, 255, 255);
         pt_w.x = pt_world[0];
@@ -148,7 +138,7 @@ public:
         pt_cam[1] = y;
         pt_cam[2] = z;
         pt_cam[3] = 1.f;
-        Vec4 pt_world = T_newestF_currF * pt_cam * 20.6128;
+        Vec4 pt_world = T_newestF_currF * pt_cam * rescale_factor;
 
         pcl::PointXYZRGB pt_w(255, 255, 255);
         pt_w.x = pt_world[0];
@@ -169,22 +159,22 @@ public:
         pt_cam[1] = y;
         pt_cam[2] = z;
         pt_cam[3] = 1.f;
-        Vec4 pt_world = T_newestF_currF * pt_cam * 20.6128;
+        Vec4 pt_world = T_newestF_currF * pt_cam * rescale_factor;
 
         pcl::PointXYZRGB pt_w(255, 255, 255);
         pt_w.x = pt_world[0];
         pt_w.y = pt_world[1];
         pt_w.z = pt_world[2];
         cloud.push_back(pt_w);
+        // std::cout << "pt_w: " << pt_w.x << " " << pt_w.y << " " << pt_w.z << " coming from " << p->u << " " << p->v
+        //           << " and p->idepth = " << idepth << std::endl;
       }
 
       // TODO: watch out, f->pointHessians is a normal ptr, there may be some dereferencing issues. Might have to make
       // copies instead. for each point, backproject into KF and transform using that KF pose
 
-      //   accumulated_pointcloud.insert(accumulated_pointcloud.end(), f->pointHessians.begin(),
-      //   f->pointHessians.end());
-    }
-    
+    } 
+        
     ROS_WARN("num marginalized points: %d\n", num_pt_marg);
 
     // TODO: extract point cloud near current pose. Maybe use KNN library? PCL?
@@ -214,10 +204,44 @@ public:
     pointcloud_pub.publish(ros_pointcloud);
   }
 
+
+  virtual void publishCamPose(FrameShell* frame, CalibHessian* HCalib) override
+  {
+    printf("OUT: Current Frame %d (time %f, internal ID %d). CameraToWorld:\n",
+           frame->incoming_id,
+           frame->timestamp,
+           frame->id);
+    std::cout << frame->camToWorld.matrix3x4() << "\n";
+
+    // permutation matrix to get world coord system (DSO in cam coord system)
+    Eigen::Matrix<double,4, 4> T_world_dso;
+    T_world_dso << 0, 0, 1, 0,
+                  -1, 0, 0, 0,
+                   0, -1, 0, 0,
+                   0, 0, 0, 1;
+
+    Eigen::Matrix<double, 4, 4> T_world_cam = T_world_dso * frame->camToWorld.matrix()/* .inverse() */;
+    ros::Time time(frame->timestamp);
+
+    tf::StampedTransform T_world_cam_msg =
+      PointMatcher_ros::eigenMatrixToStampedTransform<double>(T_world_cam, "world", "cam02", time);
+
+    tf::TransformBroadcaster br;
+    // tf::Transform transform(T_world_cam.block<3, 3>(0, 0), T_world_cam.block<3, 1>(0, 3));
+    // transform.setOrigin(tf::Vector3(msg->x, msg->y, 0.0));
+    // tf::Quaternion q;
+    // q.setRPY(0, 0, msg->theta);
+    // transform.setRotation(q);
+    br.sendTransform(T_world_cam_msg);  // TODO: change timestamp to ROS time format
+    // br.sendTransform(tf::StampedTransform(transform, frame->timestamp, "world", "cam02"));  // TODO: change timestamp to ROS time format
+  }
+
 private:
   ros::Publisher pointcloud_pub;
   ros::Publisher pointcloud_info_pub;
+  ros::Publisher cam_pose_pub;
 
+  float rescale_factor = 1.0;
   
 };
 
